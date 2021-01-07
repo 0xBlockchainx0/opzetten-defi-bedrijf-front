0e11f6976bdd6f85de528dc2d16de5fb5f7c72c4
Dylan Cornelisse <462777@student.saxion.nl>
2021-01-07 16:37:47 +0100

    updated

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -59,19 +59,19 @@ export function colors(darkMode: boolean): Colors {
     advancedBG: darkMode ? 'rgba(0,0,0,0.1)' : 'rgba(255,255,255,0.6)',
 
     //primary colors
-    primary1: darkMode ? '#2172E5' : '#ff007a',
-    primary2: darkMode ? '#3680E7' : '#FF8CC3',
-    primary3: darkMode ? '#4D8FEA' : '#FF99C9',
-    primary4: darkMode ? '#376bad70' : '#F6DDE8',
-    primary5: darkMode ? '#153d6f70' : '#FDEAF1',
+    primary1: darkMode ? '#ffcb4e' : '#ffcb4e',
+    primary2: darkMode ? '#ffcb4e' : '#ffcb4e',
+    primary3: darkMode ? '#ffcb4e' : '#ffcb4e',
+    primary4: darkMode ? '#ffcb4e' : '#ffcb4e',
+    primary5: darkMode ? '#ffcb4e' : '#f1d084',
 
     // color text
-    primaryText1: darkMode ? '#6da8ff' : '#ff007a',
+    primaryText1: darkMode ? '#ffedc4' : '#ffedc4',
 
     // secondary colors
-    secondary1: darkMode ? '#2172E5' : '#ff007a',
-    secondary2: darkMode ? '#17000b26' : '#F6DDE8',
-    secondary3: darkMode ? '#17000b26' : '#FDEAF1',
+    secondary1: darkMode ? '#2172E5' : '#ffedc4',
+    secondary2: darkMode ? '#17000b26' : '#ffedc4',
+    secondary3: darkMode ? '#17000b26' : '#ffedc4',
 
     // other
     red1: '#FF6871',
